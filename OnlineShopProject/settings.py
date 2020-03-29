b990b747b50c9e86d1ecdf4d77825d02cf1290b8
Negar-R <negar.r.78.ioi@gmail.com>
2020-03-29 16:16:28 +0430

    added media_url and media_root for display images

--- OnlineShopProject/settings.py
+++ OnlineShopProject/settings.py
@@ -154,3 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# show image
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
