26353221f48ed04ef893faf11e471f0e03c49721
Negar-R <negar.r.78.ioi@gmail.com>
2020-04-08 13:58:36 +0430

    correct the mistake

--- OnlineShopProject/settings.py
+++ OnlineShopProject/settings.py
@@ -173,6 +173,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'new.fighting.for.goal@gmail.com'
-EMAIL_HOST_PASSWORD = 'a2+b2=c2'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = '*'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
